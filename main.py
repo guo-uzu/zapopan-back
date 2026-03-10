06f7f6690e3113a54afb63fb50b99cbfbf1c5504
Alejandro Camacho Olivares <alejandro@uzu.digital>
2026-03-10 14:11:13 -0600

    feat: Dockerfile added. Added png, jpg, pdf and svg format

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import numpy as np
 app = FastAPI()
 origins = [
     "http://localhost:3000",
+    "https://atencion-al-usuario.uzu.digital",
+    "https://atencion-al-usuario.uzu.digital/",
 ]
 
 app.add_middleware(
@@ -30,6 +32,7 @@ class ChartRequest(BaseModel):
     dateFrom: str
     dateTo: str
     data: List[ReportItem]
+    formatFile: str
 
 @app.post("/api/create-chart")
 def read_root(req: ChartRequest):
@@ -46,7 +49,14 @@ def read_root(req: ChartRequest):
         ax.set_xticklabels(xAxis, rotation=45, ha="right", fontdict=font2)
         plt.tight_layout()
     buf = io.BytesIO()
-    plt.savefig(buf, format="png")
+    plt.savefig(buf, format=req.formatFile)
     buf.seek(0)
     plt.close(fig)
-    return StreamingResponse(buf)
+    if req.formatFile == "png":
+        return StreamingResponse(buf, media_type="image/png")
+    elif req.formatFile == "pdf":
+        return StreamingResponse(buf, media_type="application/pdf")
+    elif req.formatFile == "svg":
+        return StreamingResponse(buf, media_type="image/svg+xml")
+    else:
+        return StreamingResponse(buf, media_type="image/jpeg")
